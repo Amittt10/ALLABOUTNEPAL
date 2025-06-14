fe808be56fb195ea3292eb0dda0dac0aef3a18bd
unknown <bikashmhto46@gmail.com>
2025-06-15 01:02:32 +0545

    update in folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter , Routes , Route} from 'react-router-dom';
 
 
+
 import App from './App';
 
 import UserLogin from './App/UserLogin';
 import UserSignup from './App/UserSignup';
+import Footer from './Component/Footer/Footer';
+import Header from './Component/Header/Header';
+import Layout from './Component/Layout/Layout';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
@@ -15,9 +19,11 @@ root.render(
       {/* initial route  */}
      
       <Route path='/' element={<UserSignup/>}/>
-      
+      <Route path='/footer' element={<Footer/>}/>
+      <Route path='/header' element={<Header/>}/>
+      <Route path='/layout' element={<Layout/>}/>
       <Route path='/login' element={<UserLogin/>}/>
-      <Route path='/App' element={<App/>}>
+      <Route path='/app' element={<App/>}>
       </Route>
     </Routes>
   </BrowserRouter>
