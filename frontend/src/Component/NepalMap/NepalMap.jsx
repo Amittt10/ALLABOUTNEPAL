d6bed073fd61cfc7564dc80dc508ba40605e2300
Amittt10 <albert757072@gmail.com>
2025-07-11 22:52:18 +0545

    upd

--- frontend/src/Component/NepalMap/NepalMap.jsx
+++ frontend/src/Component/NepalMap/NepalMap.jsx
@@ -1,10 +1,10 @@
 import React, { useMemo } from "react";
-import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "./NepalMap.css";
 
-// Fix default marker icon URLs for Leaflet
+// Fix default Leaflet icon path issue
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
@@ -12,12 +12,12 @@ L.Icon.Default.mergeOptions({
 });
 
 export default function NepalMap({ places }) {
-  // Memoize icons for performance and safety
+  // Cache icons by thumbnail URL
   const getIcon = useMemo(() => {
     const cache = new Map();
     return (thumbnailUrl) => {
       if (!thumbnailUrl || typeof thumbnailUrl !== "string") {
-        return new L.Icon.Default(); // fallback to default if no image
+        return new L.Icon.Default(); // fallback to default icon
       }
 
       if (cache.has(thumbnailUrl)) return cache.get(thumbnailUrl);
@@ -37,15 +37,20 @@ export default function NepalMap({ places }) {
 
   return (
     <MapContainer
-      center={[28.3949, 84.124]}
-      zoom={7}
-      scrollWheelZoom={false}
-      className="nepal-map-container"
+        center={[28.3949, 84.124]}
+        zoom={7}
+        minZoom={7}
+        maxZoom={7}
+        scrollWheelZoom={false}
+        zoomControl={false}
+        doubleClickZoom={false}
+        className="nepal-map-container"
     >
+
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
       {places.map((place) => {
-        const { location, _id, title_en, thumbnail } = place;
+        const { _id, location, title_en, thumbnail } = place;
 
         if (
           location &&
@@ -55,24 +60,23 @@ export default function NepalMap({ places }) {
           const icon = getIcon(thumbnail);
 
           return (
-            <Marker key={_id} position={[location.lat, location.lng]} icon={icon}>
-              <Popup>
-                <div className="popup-content">
-                  <h3>{title_en}</h3>
-                  {thumbnail ? (
-                    <img
-                      src={thumbnail}
-                      alt={title_en}
-                      className="popup-image"
-                    />
-                  ) : (
-                    <p>No image available</p>
-                  )}
-                </div>
-              </Popup>
-              <Tooltip direction="top" offset={[0, -10]} permanent>
-                {title_en}
-              </Tooltip>
+            <Marker
+              key={_id}
+              position={[location.lat, location.lng]}
+              icon={icon}
+            >
+              <Tooltip direction="top" offset={[0, -10]} interactive>
+            <div className="tooltip-card">
+              <div className="image-wrapper">
+                <img
+                  src={thumbnail}
+                  alt={title_en}
+                />
+                <div className="image-title">{title_en}</div>
+               </div>
+              </div>
+            </Tooltip>
+
             </Marker>
           );
         }
